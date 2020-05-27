293ddbf47b15045821c0434205565a5e569cb726
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-26 20:09:28 -0700

    Add `Component.identity`
    fixes #11

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/3ce96cd.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/0e67e7b.tar.gz;
 
 in
 
