0da4d3fe62b3ad23f649bafdf1c6ff0d02acea89
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-08 12:36:12 -0700

    Build on OCaml 4.11 (#15)

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/0e67e7b.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/957c4eb.tar.gz;
 
 in
 
