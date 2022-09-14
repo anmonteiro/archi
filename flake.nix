505fda2ed9d21ef2f756dfc70c0999e6d72661e3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-09-14 12:39:37 -0700

    wip

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
   description = "Archi Nix Flake";
 
+  inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
   inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nix-filter, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
-        archiPkgs = (pkgs.callPackage ./nix { inherit pkgs; });
+        packages = pkgs.callPackage ./nix { nix-filter = nix-filter.lib; };
       in
-      rec {
-        packages = archiPkgs // { default = archiPkgs.archi; };
+      {
+        defaultPackage = packages.archi;
+        inherit packages;
         devShells = {
           default = pkgs.callPackage ./shell.nix { inherit packages; };
           release = pkgs.callPackage ./shell.nix {
